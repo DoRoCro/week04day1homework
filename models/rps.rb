f851ad6af6729d6a7d2a1c9674a645cb704de517
Douglas Crooke <douglas.crooke+codeclan@googlemail.com>
2017-02-20 20:16:51 +0000

    Class tests working

--- models/rps.rb
+++ models/rps.rb
@@ -2,26 +2,33 @@ class Rps
 
   def run(hand1, hand2)
     case hand1
-    when "rock"
-      if hand2 == "scissors"
-        return "rock wins!"
-      elsif hand2 == "paper"
-        return "paper wins!"
+      when "rock"
+        if hand2 == "scissors"
+          return "rock wins!"
+        elsif hand2 == "paper"
+          return "paper wins!"
+        else
+          return "its a draw!"
+        end
+      when "paper"
+        if hand2 == "rock"
+          return "paper wins!"
+        elsif hand2 == "scissors"
+          return "scissors wins!"
+        else
+          return "its a draw!"
+        end
+      when "scissors"
+        if hand2 == "rock"
+          return "rock wins!"
+        elsif hand2 == "paper"
+          return "scissors wins!"
+        else
+          return "its a draw!"
+        end
       else
-        return "its a draw!"
-      end
-    when "paper"
-      if hand2 == "rock"
-        return "paper wins!"
-      elsif hand2 == "scissors"
-        return "scissors wins!"
-      else
-        return "its a draw!"
-      end
-        
-
+        return "error in input - try again!"
     end
   end
-
-
+  
 end
