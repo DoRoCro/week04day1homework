3825a75bae8cce4d9c39dceda602582ecdd4a7cd
Douglas Crooke <douglas.crooke+codeclan@googlemail.com>
2017-02-20 21:31:42 +0000

    Use and erb page for results

--- controller.rb
+++ controller.rb
@@ -1,9 +1,15 @@
 require_relative 'models/rps.rb'
 require 'sinatra' 
 require 'sinatra/contrib/all' if development?  
-require('pry')
+require 'pry'
+
+# get "/rps/:hand1/:hand2" do
+#   rps = Rps.new(params[:hand1], params[:hand2])
+#   return rps.run
+# end
 
 get "/rps/:hand1/:hand2" do
-  rps = Rps.new(params[:hand1], params[:hand2])
-  return rps.run
+  rps = Rps.new params[:hand1], params[:hand2]
+  @winner = rps.run
+  erb :rps
 end
