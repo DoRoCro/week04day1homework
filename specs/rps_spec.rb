498ade79012ec34eaf6f8b60053c8420578c42da
Douglas Crooke <douglas.crooke+codeclan@googlemail.com>
2017-02-20 20:55:38 +0000

    check reverse order Class tests working

--- specs/rps_spec.rb
+++ specs/rps_spec.rb
@@ -10,14 +10,17 @@ class TestRPS < Minitest::Test
 
   def test_rps_rock_wins
     assert_equal 'rock wins!', @rps.run("rock", "scissors")
+    assert_equal 'rock wins!', @rps.run("scissors", "rock")
   end
 
   def test_rps_scissors_wins
     assert_equal 'scissors wins!', @rps.run("scissors", "paper") 
+    assert_equal 'scissors wins!', @rps.run("paper", "scissors") 
   end
 
   def test_rps_paper_wins
     assert_equal 'paper wins!', @rps.run("paper", "rock")
+    assert_equal 'paper wins!', @rps.run("rock", "paper")
   end
 
 
